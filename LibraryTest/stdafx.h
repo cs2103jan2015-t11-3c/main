bb4f9745184775b2388d183cbcfa24255cfd6cb4
Alvin <a0116006@nus.edu.sg>
2015-04-07 17:51:06 +0800

    unit testing template

---
+++ LibraryTest/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+// Headers for CppUnitTest
+#include "CppUnitTest.h"
+
+// TODO: reference additional headers your program requires here
+#include "default.h"
+#include "logic.h"
+#include "parser.h"
+#include "recurringTask.h"
+#include "storage.h"
+#include "Task.h"
+#include "undo.h"
