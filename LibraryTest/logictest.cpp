34abaf8e6521600db5ed193824d628130a04007a
Alvin <a0116006@nus.edu.sg>
2015-04-09 16:23:58 +0800

    alvin

---
+++ LibraryTest/logictest.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace LibraryTest
+{		
+	TEST_CLASS(parsertest)
+	{
+	public:
+		
+		TEST_METHOD(TestValidCommand)
+		{
+		    parser p;
+			string testCommand = "add";
+			string testDescription="shopping";
+			int expected = 1;
+			int test=p.isValidCommand(testCommand,testDescription);
+			Assert::AreEqual (test, expected);
+		
+		}
+
+		TEST_METHOD(TestconvertAlphabetMonthToInteger)
+		{
+		     parser p;
+			int testString = p.convertAlphabetMonthToInteger("jan");
+			int expected = 1;
+			Assert::AreEqual (testString, expected);
+		
+		}
+		
+	};
+}
