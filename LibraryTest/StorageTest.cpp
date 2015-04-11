ef23fa7ef017c64472098d4e106bba0c7679d1ef
wyl2015 <wangyalan.01@hotmail.com>
2015-04-11 15:59:37 +0800

    unit test

--- LibraryTest/StorageTest.cpp
+++ LibraryTest/StorageTest.cpp
@@ -31,6 +31,35 @@ namespace UnitTest
 		}
 
 
+		TEST_METHOD(TestisDeadlineDuplicated)
+		{
+			storage *s=new storage;
+			task NewTask("shopping");
+			vector <task> Itemlist;
+		    NewTask.addDeadlineItem(27,04,2015,1000);
+			defaultclass default;
+			string description;
+			default.addDeadlineTask("add meeting /by 1000 on 27/04/2015",Itemlist,s);
+			bool expected=true;
+			bool duplicated=s->isTimeClashed(NewTask,Itemlist);
+			Assert::AreEqual (expected,duplicated);
+		}
+
+		TEST_METHOD(TestIsTimeClashed)
+		{
+			storage *s=new storage;
+			task NewTask("shopping");
+			vector <task> Itemlist;
+		    NewTask.addTimedItem(27,04,2015,900,27,04,2015,1100);
+			defaultclass default;
+			string description;
+			default.addTimedTask("add meeting /from 1000 on 27/04/2015 /to 1300 on 27/04/2015",Itemlist,s);
+			bool expected=true;
+			bool clashed=s->isTimeClashed(NewTask,Itemlist);
+			Assert::AreEqual (expected,clashed);
+		}
+
+
 
 		TEST_METHOD(TestfileExistance)
 		{
