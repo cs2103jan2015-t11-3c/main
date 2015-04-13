4cea24c1640886986a04f5410e9352a2536ace71
wyl2015 <wangyalan.01@hotmail.com>
2015-04-13 18:37:15 +0800

    minor changes

--- project/recurringTask.h
+++ project/recurringTask.h
@@ -9,39 +9,33 @@ private:
 	string description;
 	int s_time;
 	int e_time;
+
 public:
 	recurringTask(void);
-	recurringTask(string text, int stime ,int etime);
+	recurringTask(string , int  ,int );
 	~recurringTask(void);
 
-	void AddRecurring(int n,string recurType,int e_date,int e_month,int e_year,int s_date,int s_month,int s_year,string type,vector<task> &toDoList);
+	void AddRecurring(int ,string ,int ,int ,int ,int ,int ,int ,string ,vector<task> &);
 
-	void DailyRec(int n,int e_date,int e_month,int e_year,int s_date,int s_month,int s_year,string type,vector<task> &toDoList);
-	void WeeklyRec(int n,int e_date,int e_month,int e_year,int s_date,int s_month,int s_year,string type,vector<task> &toDoList);
-	void MonthlyRec(int n,int e_date,int e_month,int e_year,int s_date,int s_month,int s_year,string type,vector<task> &toDoList);
-	void YearlyRec(int n,int e_date,int e_month,int e_year,int s_date,int s_month,int s_year,string type,vector<task> &toDoList);
-	bool isValidforMoreDays(int month);
-	void addRecDeadline( int e_date, int e_month, int e_year, vector<task> &toDoList);
-	void addRecTimed(int e_date,int e_month,int e_year,int s_date,int s_month,int s_year,vector<task> &toDoList);
+	void DailyRec(int ,int ,int ,int ,int ,int ,int ,string ,vector<task> &);
+	void WeeklyRec(int ,int ,int ,int ,int ,int ,int ,string ,vector<task> &);
+	void MonthlyRec(int ,int ,int ,int ,int ,int ,int ,string ,vector<task> &);
+	void YearlyRec(int ,int ,int ,int ,int ,int ,int ,string ,vector<task> &);
 
-	int getNumDays(int month,int year);
-
-	void addTodifferentType(string type, int e_date,int e_month,int e_year,int s_date,int s_month,int s_year,vector<task> &toDoList);
-
-    void ChangeWeekDeadline(int &e_date,int &e_month,int &e_year,int s_date,int s_month,int s_year,string type,vector<task> &toDoList);
-    void ChangeMonthDeadline(int &e_date,int &e_month,int &e_year,int s_date,int s_month,int s_year,string type,vector<task> &toDoList);
-
-
-
-	void moveToNextMonth(int &date, int &month, int &year);
-	void moveToNextYear(int &date, int &month, int &year);
-
-	int getDaysInterval(int sday,int smonth,int syear,int eday, int emonth, int eyear);
-	int DayInYear(int year, int month, int day);
-
-	bool MonthRecValid(int e_date,int e_month,int e_year,int s_date,int s_month,int s_year);
-	bool WeekRecValid(int e_date,int e_month,int e_year,int s_date,int s_month,int s_year);
-	bool DayRecValid(int e_date,int e_month,int e_year,int s_date,int s_month,int s_year);
+	bool isValidforMoreDays(int );
+	void addRecDeadline( int , int , int , vector<task> &);
+	void addRecTimed(int ,int ,int ,int ,int ,int ,vector<task> &);
+	int getNumDays(int ,int );
+	void addTodifferentType(string , int ,int ,int ,int ,int ,int ,vector<task> &);
+    void ChangeWeekDeadline(int &,int &,int &,int ,int ,int ,string ,vector<task> &);
+    void ChangeMonthDeadline(int &,int &,int &,int ,int ,int ,string ,vector<task> &);
+	void moveToNextMonth(int &, int &, int &);
+	void moveToNextYear(int &, int &, int &);
+	int getDaysInterval(int ,int ,int ,int , int , int );
+	int DayInYear(int , int , int );
+	bool MonthRecValid(int ,int ,int ,int ,int ,int );
+	bool WeekRecValid(int ,int ,int ,int ,int ,int );
+	bool DayRecValid(int ,int ,int ,int ,int ,int );
 
 
 
